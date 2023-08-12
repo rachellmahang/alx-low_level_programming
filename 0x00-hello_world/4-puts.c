3f4c043b747c8af812a22f0f852d2c885dcfa155
rachellmahang <rachellmahang@gmail.com>
2023-08-11 18:29:35 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point.
+ *
+ * Description: this is my c code
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
