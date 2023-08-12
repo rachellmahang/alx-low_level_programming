963309f07e3136ca3eff8401c13f188867e9a9a9
rachellmahang <rachellmahang@gmail.com>
2023-08-11 18:44:17 -0700

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting erea
+ *
+ * Description: this is my 2nd c code
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
