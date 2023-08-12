4d6995a94af6bf44d7f6e179e13f0e82f7144faa
rachellmahang <rachellmahang@gmail.com>
2023-08-11 19:39:32 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - begining
+ *
+ * Description: Size is not bigger than erea
+ * Return: 0 is always returned
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
