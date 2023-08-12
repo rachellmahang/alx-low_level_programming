1d89fbcafc63d0750111fc7637979cc825e86581
rachellmahang <rachellmahang@gmail.com>
2023-08-11 20:13:59 -0700

    hello

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - A C program that prints exactly
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+		return (1);
+}
