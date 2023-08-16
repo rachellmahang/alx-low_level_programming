2a79a4eb8357baa98b0dc6b3f06ec24c95e395e0
rachellmahang <rachellmahang@gmail.com>
2023-08-16 10:41:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - is the main function
+ * @n: is the integer value
+ * Return: Always as specified
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
