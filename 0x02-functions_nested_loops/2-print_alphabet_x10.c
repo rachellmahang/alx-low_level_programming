02bffa5ae2197c1b9c6f347bf34c772210f96af0
rachellmahang <rachellmahang@gmail.com>
2023-08-16 10:04:28 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - will print all lowercase alphabet
+ * Description - Alphabet code is string
+ * Return: Check with my code
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
