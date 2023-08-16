456a4c2037c3e3083692d52a1b653f09627dcd86
rachellmahang <rachellmahang@gmail.com>
2023-08-16 10:52:17 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - is the main point
+ * @n: is the integer code
+ * Return: Always as specified
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
