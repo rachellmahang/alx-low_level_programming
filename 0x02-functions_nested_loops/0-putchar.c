e72f6734e48fcf452ac17529cf58f1fa8c606641
rachellmahang <rachellmahang@gmail.com>
2023-08-15 17:08:39 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - point_putchar
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	-putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
+	return (0);
+}
