86f1043e5aa2aeada58c81b4ef6796a96518e814
rachellmahang <rachellmahang@gmail.com>
2023-08-16 11:33:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - start point
+ * @a: is the first integer
+ * @b: is the second integer
+ * Return: Always as specified
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
