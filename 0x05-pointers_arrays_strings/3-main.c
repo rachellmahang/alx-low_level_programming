9688b3b688b69d666555200d82e6eae27c74d63f
rachellmahang <rachellmahang@gmail.com>
2023-08-22 18:38:40 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - a function that prints out a string followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
