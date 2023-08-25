264f4904eb850326a760b60ded204a2fd0f4ab5f
rachellmahang <rachellmahang@gmail.com>
2023-08-24 17:01:50 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that concatenates two strings.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char s1[98] = "hello";
+	char s2[] = "world!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
+}
