027603d5296e06a5813e6c6f58020d0ec1e8674a
rachellmahang <rachellmahang@gmail.com>
2023-08-28 11:09:01 -0700

    pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,11 +12,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; 1++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", a[1][j]);
+			printf("%c ", a[i][j]);
 		}
 		printf("\n");
 	}
