06b1da5a2ec16540afb6e18badbbced324a983e3
rachellmahang <rachellmahang@gmail.com>
2023-08-28 08:28:52 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "sttded"
+
+/**
+ *  _strpn -  a function that gets the length of a prefix substring.
+ *  @s: initial segment
+ *  @accept: value of byte
+ *  Return: as mentioned
+ */
+size_t _strpn(char *s, char *accept)
+{
+	size_t len = 0;
+
+	while (*s && strchr(accept, *s++))
+		len++;
+	return (len);
+}
