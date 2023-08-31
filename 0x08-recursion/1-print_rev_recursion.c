10b35e3fa456701f213c89607bbb3ff05924d6f5
rachellmahang <rachellmahang@gmail.com>
2023-08-30 17:52:27 -0700

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		putchar(*s);
+		return;
 	}
-	putchar('\n');
-
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
