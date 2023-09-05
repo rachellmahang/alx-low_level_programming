c894108de9bb8a152c55a72b86d4565a119057e0
rachellmahang <rachellmahang@gmail.com>
2023-09-05 07:13:49 -0700

    libraries

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - is the function used
+ * @argc: is the argument to main for numbers
+ * @argv: is the first element
+ * Return: as specified
+ */
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
