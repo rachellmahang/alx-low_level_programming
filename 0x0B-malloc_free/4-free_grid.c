df8acd65e71e8b2c620cfdb6b63930fd12a7925b
rachellmahang <rachellmahang@gmail.com>
2023-09-05 12:39:50 -0700

    malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,13 +11,13 @@ void free_grid(int **grid, int height)
 {
 	int i = 0;
 
-	if (grit == NULL || height <= 0)
+	if (grid == NULL || height <= 0)
 	{
 		return;
 	}
 	while (i < height)
 	{
-		free(grit[i]);
+		free(grid[i]);
 		i++;
 	}
 	free(grid);
