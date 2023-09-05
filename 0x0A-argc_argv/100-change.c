6ca106a56223dc76bddc3b3551a5cc982a319f30
rachellmahang <rachellmahang@gmail.com>
2023-09-05 07:52:18 -0700

    argc

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - is the function that prints minimum coins
+ * @argc: is the coin
+ * @argv: is the array
+ * Return: as mentioned
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins += cents / 25;
+	cents %= 25;
+
+	coins += cents / 10;
+	cents %= 10;
+
+	coins += cents / 5;
+	cents %= 5;
+
+	coins += cents / 2;
+	cents %= 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+
+	return (0);
+}
+
