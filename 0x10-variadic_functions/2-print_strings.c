76b93343ee2e66b434c40fbfdbacd4cd85b00605
rachellmahang <rachellmahang@gmail.com>
2023-09-14 08:33:59 -0700

    pointers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include < stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - a function that prints strings
+ * @separator: is the pointer
+ * @n: value
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char*);
+		printf("%s", str ? str : "(NULL)");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	var_end(args);
+}
