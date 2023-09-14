61852396b6a170b4947aa228624ac6b2f89bcd81
rachellmahang <rachellmahang@gmail.com>
2023-09-14 07:34:56 -0700

    pointers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variatic_function.h"
+
+/**
+ * print_numbers - a function that print numbers
+ * @separator: is the pointer
+ * @n: is the value
+ */
+void print_numbers(const char separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
