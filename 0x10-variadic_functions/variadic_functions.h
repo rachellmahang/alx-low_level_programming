90b4023d7e3d6a2fcfc2561fe4acffe23b2487b2
rachellmahang <rachellmahang@gmail.com>
2023-09-14 17:40:46 -0700

    pointers

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+
+#include <stdarg.h>
+
+typedef struct print_d
+{
+	char *c;
+	void (*f_pr)(va_list a);
+} print_d;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
