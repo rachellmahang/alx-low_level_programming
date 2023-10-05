a59644fc2893a8c27ea0784dacfcf31e57e0b0c6
rachellmahang <rachellmahang@gmail.com>
2023-10-05 14:32:24 -0700

    textfiles

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,35 @@
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
 /**
- * create_file - a function that appends text at the end of a file
+ * create_file - is the function that create a file
  * @filename: is the pointer
- * @text_content: is constant
+ * @text_content: is the pointer
  * Return: as mentioned
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t write_count;
-	size_t length = 0;
+	int i;
+	ssize_t write_text;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (i == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[length])
-			length++;
-
-		write_count = write(fd, text_content, length);
-		if (write_count == -1 || write_count != length)
+		write_text = write(i, text_content, strlen(text_content));
+		if (write_text == -1)
 		{
-			close(fd);
+			close(i);
 			return (-1);
 		}
 	}
 
-	close(fd);
+	close(i);
 	return (1);
 }
