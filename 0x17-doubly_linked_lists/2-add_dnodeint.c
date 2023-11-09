6bdd515220b1c99aea35bc75ac97fbcde9d6f288
rachellmahang <rachellmahang@gmail.com>
2023-11-09 13:29:37 -0800

    boubly linked

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - a function that adds a new node
+ * @head: the head of the list
+ * @n: is the interger
+ * new_node: is the added node
+ * Return: as mentioned
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head != NULL)
+	(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
+}
