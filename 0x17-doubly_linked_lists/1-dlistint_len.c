8aad1a04a44d4b649c02eb1f2f673fa6f96f8c92
rachellmahang <rachellmahang@gmail.com>
2023-11-09 13:18:14 -0800

    boubly linked

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,18 +3,18 @@
 
 /**
  * dlistint_len - a function that return the number of elements
- * @head: head of the list
+ * @h: head of the list
  * Return: as mentioned
  */
 
-size_t dlistint_len(dlistint_t *head)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (head != NULL)
+	while (h != NULL)
 	{
 		count++;
-		head = head->next;
+		h = h->next;
 	}
 
 	return (count);
