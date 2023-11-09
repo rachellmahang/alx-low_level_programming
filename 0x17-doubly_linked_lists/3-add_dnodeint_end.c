bd304a2dbcaa9553732f95389311243180723b94
rachellmahang <rachellmahang@gmail.com>
2023-11-09 14:02:31 -0800

    boubly linked

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
-		return (NULL);
+	return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
